d07a0fae84a1976462a86077f41a15ad7435bd99
SimpleDoodle <48078201+SimpleDoodle@users.noreply.github.com>
2024-07-21 12:55:41 -0700

    Thread Robot Race

--- src/_02_Advanced_Robot_Race/AdvancedRobotRace.java
+++ src/_02_Advanced_Robot_Race/AdvancedRobotRace.java
@@ -1,6 +1,35 @@
 package _02_Advanced_Robot_Race;
 
+import java.util.Random;
+
+import org.jointheleague.graphical.robot.Robot;
+
 public class AdvancedRobotRace {
 	// Re-do the robot race recipe from level 3 module 0. 
 	// This time, use threads to make all of the robots go at the same time.
+	public static void main(String[] args) {
+		int x = 100;
+		int[] y = {800, 800, 800, 800, 800};
+		Robot[] robs = new Robot[5];
+		Thread[] race = new Thread[5];
+		for (int i = 0; i < robs.length; i++) {
+			robs[i] = new Robot(x,y[i]);
+			x+=200;
+		}
+		boolean raceFinished = false;
+		Random rand = new Random();
+		while(raceFinished = false) {
+			for (int i = 0; i < race.length; i++) {
+				int d = rand.nextInt(50);
+				y[i] += d;
+				robs[i].move(d);
+				if (y[i] >= 100) {
+					raceFinished = true;
+				}
+			}
+		}
+		for (Thread thread : race) {
+			thread.start();
+		}
+	}
 }
