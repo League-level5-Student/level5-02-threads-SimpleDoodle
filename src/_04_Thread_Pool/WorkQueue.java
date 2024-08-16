f5fe5d2b9c49be7d561307410129513da025c145
League Student <league@iMac-39.attlocal.net>
2024-08-15 18:32:58 -0700

    ThreadPools and Sync

--- src/_04_Thread_Pool/WorkQueue.java
+++ src/_04_Thread_Pool/WorkQueue.java
@@ -1,28 +1,81 @@
 package _04_Thread_Pool;
 
+import java.lang.Thread.State;
+import java.util.ArrayDeque;
+import java.util.Iterator;
+
 public class WorkQueue implements Runnable {
+	private ArrayDeque<Job> jobQueue = new ArrayDeque<>();
 	private Thread[] threads;
-	private boolean isRunning = true;
-	
+	private volatile boolean isRunning = true;
+
 	public WorkQueue() {
-		int totalThreads = Runtime.getRuntime().availableProcessors()-1;
+		int totalThreads = Runtime.getRuntime().availableProcessors() - 1;
 		threads = new Thread[totalThreads];
 		for (int i = 0; i < threads.length; i++) {
 			threads[i] = new Thread(this);
 			threads[i].start();
 		}
 	}
+
+	public boolean performJob() {
+		Job j = null;
+		synchronized (jobQueue) {
+			if (!jobQueue.isEmpty()) {
+				j = jobQueue.remove();
+
+			}
+		}
+		if (j != null) {
+			j.perform();
+			return true;
+		} else {
+			return false;
+		}
+	}
+	public void completeAllJobs(){
+		while(!jobQueue.isEmpty()) {
+			performJob();
+		}
+		for (int i = 0; i < threads.length; i++) {
+			if (threads[i].getState() != State.WAITING) {
+				i = -1;
+			}
+		}
+	}
+
 	public int getThreadCount() {
 		return threads.length;
 	}
+
 	@Override
 	public void run() {
-		while(isRunning) {
-			System.out.println("Output from thread # " + Thread.currentThread().getId());
+		while (isRunning) {
+			if (!performJob()) {
+				synchronized (jobQueue) {
+					try {
+						jobQueue.wait();
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			}
 		}
 	}
+
 	public void shutdown() {
+		completeAllJobs();
 		isRunning = false;
+		synchronized (jobQueue) {
+			jobQueue.notify();
+		}
+	}
+
+	public void addJob(Job j) {
+		synchronized (jobQueue) {
+			jobQueue.add(j);
+			jobQueue.notify();
+		}
 	}
 
 }
